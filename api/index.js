6fe93381c37612a79ef2c71747c7c5921eddc5ed
Puneet Azad <puneet.azad@msit.in>
2025-01-11 00:47:09 +0530

    add redux toolkit

--- api/index.js
+++ api/index.js
@@ -4,8 +4,6 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
-
-
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -16,9 +14,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 
 const app = express();
-
 app.use(express.json());
-
 
 app.listen(3000, () => {console.log('Server is running on port 3000!!'); }
 );
